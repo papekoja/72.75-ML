8d710b8b28d4efafc0dd949d5472fdae80b737d4
William <william.reit@gmail.com>
2023-08-30 01:08:58 -0300

    m

--- trabajo-2/task3william.py
+++ trabajo-2/task3william.py
@@ -21,3 +21,22 @@ p_not_admitted_given_rank_1 = total_not_admitted_given_rank_1 / total_rank_1
 p_rank_1_given_not_admitted = (p_not_admitted_given_rank_1 * p_rank_1) / p_not_admitted
 
 print(f'P(rank=1|not admitted) = {p_rank_1_given_not_admitted}')
+
+# Calculate the conditional probability
+total_rank_2_gre_450_gpa_35 = data_df[
+    (data_df['rank'] == 2) &
+    (data_df['gre'] < 500) &
+    (data_df['gpa'] >= 3) &
+    (data_df['admit'] == 1)
+].shape[0]
+
+total_rank_2_gre_450_gpa_35_total = data_df[
+    (data_df['rank'] == 2) &
+    (data_df['gre'] < 500) &
+    (data_df['gpa'] >= 3)
+].shape[0]
+
+# Calculate the conditional probability
+p_admitted_given_rank_2_gre_450_gpa_35 = total_rank_2_gre_450_gpa_35 / total_rank_2_gre_450_gpa_35_total
+
+print("Probability of Admitted for Rank 2, GRE = 450, GPA = 3.5:", p_admitted_given_rank_2_gre_450_gpa_35)
