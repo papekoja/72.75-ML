e162344c2102f7fc7b554205103575968215f7e3
William <william.reit@gmail.com>
2023-08-30 01:55:15 -0300

    m

--- trabajo-2/task3william.py
+++ trabajo-2/task3william.py
@@ -10,15 +10,16 @@ data_df = pd.read_csv(csv_file_path)
 total_rows = len(data_df)
 total_rank_1 = data_df[data_df['rank'] == 1].shape[0]
 total_not_admitted = data_df[data_df['admit'] == 0].shape[0]
+total_admitted = data_df[data_df['admit'] == 1].shape[0]
 total_not_admitted_given_rank_1 = data_df[(data_df['rank'] == 1) & (data_df['admit'] == 0)].shape[0]
 
 # Calculate probabilities
 p_rank_1 = total_rank_1 / total_rows
 p_not_admitted = total_not_admitted / total_rows
-p_not_admitted_given_rank_1 = total_not_admitted_given_rank_1 / total_rank_1
+p_rank_1_given_not_admitted = total_not_admitted_given_rank_1 / total_not_admitted
 
 # Apply Bayes' theorem
-p_rank_1_given_not_admitted = (p_not_admitted_given_rank_1 * p_rank_1) / p_not_admitted
+p_rank_1_given_not_admitted = (p_rank_1_given_not_admitted * p_not_admitted) / p_rank_1
 
 print(f'P(rank=1|not admitted) = {p_rank_1_given_not_admitted}')
 
@@ -36,7 +37,7 @@ total_rank_2_gre_450_gpa_35_total = data_df[
     (data_df['gpa'] >= 3)
 ].shape[0]
 
-# Calculate the conditional probability
-p_admitted_given_rank_2_gre_450_gpa_35 = total_rank_2_gre_450_gpa_35 / total_rank_2_gre_450_gpa_35_total
+p_admitted_given_rank_2 = total_rank_2_gre_450_gpa_35 / total_admitted
+p_admitted_given_rank_2_gre_450_gpa_35 = (p_admitted_given_rank_2 * total_admitted) / total_rank_2_gre_450_gpa_35_total
 
 print("Probability of Admitted for Rank 2, GRE = 450, GPA = 3.5:", p_admitted_given_rank_2_gre_450_gpa_35)
