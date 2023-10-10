3e9deae87c320dcdb0a0e292b274030bf636d54d
BillieSeffer <90696057+BillieSeffer@users.noreply.github.com>
2023-10-10 12:52:47 -0300

    progress

--- TP3/part-2/main.py
+++ TP3/part-2/main.py
@@ -4,7 +4,7 @@ from sklearn import svm
 from sklearn.metrics import confusion_matrix, classification_report
 import psutil
 import os
-from sklearn.externals import joblib
+import joblib
 
 p = psutil.Process(os.getpid())
 p.nice(psutil.HIGH_PRIORITY_CLASS)
@@ -37,22 +37,30 @@ def classify_image(image, clf):
     # Map predictions to colors
     # For this example, let's use red, green, and blue for the three categories
     color_map = {
-        0: [255, 0, 0],  # Red
-        1: [0, 255, 0],  # Green
-        2: [0, 0, 255]   # Blue
+        0: [255, 0, 0],  # Red = Cow
+        1: [0, 255, 0],  # Green = Grass
+        2: [0, 0, 255]   # Blue = Sky
     }
     colored_output = np.array([color_map[category] for category in predicted_categories])
 
     # Reshape the colored output to match the original image shape
-    output_image = colored_output.reshape(image.shape)
+    output_image = None
+    if is_rgb:
+        output_image = colored_output.reshape(image.shape)
+    else:
+        output_image = colored_output.reshape(image.shape[0], image.shape[1], 3)
+
 
     return output_image
 
+is_rgb = True
+
+read_mode = cv2.IMREAD_COLOR if is_rgb else cv2.IMREAD_GRAYSCALE
 # Load images
-vaca = cv2.imread('TP3\img/vaca.jpeg', cv2.IMREAD_GRAYSCALE)
-pasto = cv2.imread('TP3\img\pasto.jpeg', cv2.IMREAD_GRAYSCALE)
-cielo = cv2.imread('TP3\img\cielo.jpeg', cv2.IMREAD_GRAYSCALE)
-cow = cv2.imread('TP3\img\cow.jpeg', cv2.IMREAD_GRAYSCALE)
+vaca = cv2.imread('TP3\img/vaca.jpeg', read_mode)
+pasto = cv2.imread('TP3\img\pasto.jpeg', read_mode)
+cielo = cv2.imread('TP3\img\cielo.jpeg', read_mode)
+cow = cv2.imread('TP3\img\cow.jpeg', read_mode)
 
 # Create image patches of each image
 vaca_patches = extract_patches_rgb(vaca)
@@ -69,28 +77,38 @@ cielo_labels = np.ones(len(cielo_patches)) * 2
 train_patches = np.concatenate((vaca_patches, pasto_patches, cielo_patches))
 train_labels = np.concatenate((vaca_labels, pasto_labels, cielo_labels))
 
+print(train_labels)
+
 # Shuffle dataset
 shuffled_indices = np.random.permutation(len(train_patches))
 patches_set = train_patches[shuffled_indices]
 labels_set = train_labels[shuffled_indices]
 
+#print("Dataset size:", len(patches_set))
+patches_subset = patches_set[:10000]
+labels_subset = labels_set[:10000]
+
 # Split dataset into train and test
 train_quota = 0.8
-train_patches = patches_set[:int(len(patches_set) * train_quota)]
-train_labels = labels_set[:int(len(labels_set) * train_quota)]
+train_patches = patches_subset[:int(len(patches_subset) * train_quota)]
+train_labels = labels_subset[:int(len(labels_subset) * train_quota)]
 
-test_patches = patches_set[int(len(patches_set) * train_quota):]
-test_labels = labels_set[int(len(labels_set) * train_quota):]
+test_patches = patches_subset[int(len(patches_subset) * train_quota):]
+test_labels = labels_subset[int(len(labels_subset) * train_quota):]
 
 # Train the SVM
 print("Training SVM...")
 clf = svm.SVC(kernel='linear') # linear, poly, rbf, sigmoid
 clf.fit(train_patches, train_labels)
 
-# Store trained model
+# Store or load trained model
 model_name = "linear_C_1"
-filename = f"{model_name}_svm_model.pkl"
+rgb_text = "_rgb" if is_rgb else ""
+filename = f"{model_name}_svm_model{rgb_text}.pkl"
 joblib.dump(clf, filename)
+# Load model
+#clf = joblib.load(filename)
+
 
 # Predict on test images and create confusion matrix
 print("Testing SVM...")
@@ -110,5 +128,6 @@ print("Predicting on a new image...")
 output_image = classify_image(cow, clf)
 
 # Show the output image
+output_image = np.clip(output_image, 0, 255).astype(np.uint8)
 cv2.imshow('Output', output_image)
 cv2.waitKey(0) """
