3c088518bd52f391c1b14ffb1191f6487c68e68a
BillieSeffer <90696057+BillieSeffer@users.noreply.github.com>
2023-10-10 11:31:24 -0300

    progress

--- TP3/part-2/main.py
+++ TP3/part-2/main.py
@@ -1,70 +1,52 @@
-import random
-import cv2
 import numpy as np
+import cv2
+from sklearn import svm
+from sklearn.metrics import confusion_matrix, classification_report
+import psutil
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
+from sklearn.externals import joblib
 
-def add_gaussian_noise_to_grayscale(image, mean=0, sigma=25):
-    """Add Gaussian noise to a grayscale image.
-    
-    Args:
-        image (np.array): Original grayscale image.
-        mean (float): Mean of the Gaussian distribution.
-        sigma (float): Standard deviation of the Gaussian distribution.
-        
+p = psutil.Process(os.getpid())
+p.nice(psutil.HIGH_PRIORITY_CLASS)
+
+def extract_patches_rgb(img, patch_size=7):
+    """
+    Extract local patches around each pixel in the RGB image.
+
     Returns:
-        np.array: Grayscale image with added Gaussian noise.
+    - np.array: Array of flattened patches.
     """
-    row, col = image.shape
-    gauss = np.random.normal(mean, sigma, (row, col))
-    gauss = gauss.reshape(row, col)
-    noisy_image = image + gauss
-    
-    # Ensure the pixel values are within the valid range [0, 255]
-    noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
-    
-    return noisy_image
+    half_size = patch_size // 2
+    padded_img = cv2.copyMakeBorder(img, half_size, half_size, half_size, half_size, cv2.BORDER_REFLECT)
+    patches = []
 
-def create_gaussian_noise_dataset(image, n_samples=100, mean=0, sigma=25):
-    """Create a dataset of noisy images.
-    
-    Args:
-        image (np.array): Original grayscale image.
-        n_samples (int): Number of noisy images to generate.
-        mean (float): Mean of the Gaussian distribution.
-        sigma (float): Standard deviation of the Gaussian distribution.
-        
-    Returns:
-        np.array: Dataset of noisy images.
-    """
-    dataset = []
-    for _ in range(n_samples):
-        noisy_image = add_gaussian_noise_to_grayscale(image, mean, sigma)
-        dataset.append(noisy_image)
-    return np.array(dataset)
+    for y in range(half_size, padded_img.shape[0] - half_size):
+        for x in range(half_size, padded_img.shape[1] - half_size):
+            patch = padded_img[y-half_size:y+half_size+1, x-half_size:x+half_size+1]
+            patches.append(patch.flatten())
 
-def random_crop(image, crop_height, crop_width):
-    """
-    Randomly crop an image to the specified height and width.
-    
-    Args:
-    - image (np.array): Original image.
-    - crop_height (int): Height of the cropped region.
-    - crop_width (int): Width of the cropped region.
-    
-    Returns:
-    - np.array: Cropped image.
-    """
-    if crop_height > image.shape[0] or crop_width > image.shape[1]:
-        raise ValueError("Cropped dimensions are larger than image dimensions!")
-    
-    y = np.random.randint(0, image.shape[0] - crop_height)
-    x = np.random.randint(0, image.shape[1] - crop_width)
+    return np.array(patches)
 
-    cropped_image = image[y:y+crop_height, x:x+crop_width]
+def classify_image(image, clf):
+    # Extract features for prediction
+    patches = extract_patches_rgb(image)
 
-    return cropped_image
+    # Predict categories
+    predicted_categories = clf.predict(patches)
+
+    # Map predictions to colors
+    # For this example, let's use red, green, and blue for the three categories
+    color_map = {
+        0: [255, 0, 0],  # Red
+        1: [0, 255, 0],  # Green
+        2: [0, 0, 255]   # Blue
+    }
+    colored_output = np.array([color_map[category] for category in predicted_categories])
+
+    # Reshape the colored output to match the original image shape
+    output_image = colored_output.reshape(image.shape)
+
+    return output_image
 
 # Load images
 vaca = cv2.imread('TP3\img/vaca.jpeg', cv2.IMREAD_GRAYSCALE)
@@ -72,37 +54,61 @@ pasto = cv2.imread('TP3\img\pasto.jpeg', cv2.IMREAD_GRAYSCALE)
 cielo = cv2.imread('TP3\img\cielo.jpeg', cv2.IMREAD_GRAYSCALE)
 cow = cv2.imread('TP3\img\cow.jpeg', cv2.IMREAD_GRAYSCALE)
 
-# Create a dataset of noisy images
-print("Creating dataset of noisy images..."	)
-vaca_dataset = create_gaussian_noise_dataset(vaca)
-pasto_dataset = create_gaussian_noise_dataset(pasto)
-cielo_dataset = create_gaussian_noise_dataset(cielo)
+# Create image patches of each image
+vaca_patches = extract_patches_rgb(vaca)
+pasto_patches = extract_patches_rgb(pasto)
+cielo_patches = extract_patches_rgb(cielo)
+cow_patches = extract_patches_rgb(cow)
 
-# Random crop all images in the datasets
-print("Randomly cropping images...")
-vaca_cropped_dataset = []
-for i in range(len(vaca_dataset)):
-    vaca_cropped_dataset.append(random_crop(vaca_dataset[i], 140, 140))
-
-pasto_cropped_dataset = []
-for i in range(len(pasto_dataset)):
-    pasto_cropped_dataset.append(random_crop(pasto_dataset[i], 140, 140))
-
-cielo_cropped_dataset = []
-for i in range(len(cielo_dataset)):
-    cielo_cropped_dataset.append(random_crop(cielo_dataset[i], 140, 140))
-
-# Flatten all the images in the datasets
-vaca_cropped_dataset = [image_vector.flatten() for image_vector in vaca_cropped_dataset]
-pasto_cropped_dataset = [image_vector.flatten() for image_vector in pasto_cropped_dataset]
-cielo_cropped_dataset = [image_vector.flatten() for image_vector in cielo_cropped_dataset]
+# Add labels to each dataset
+vaca_labels = np.zeros(len(vaca_patches))
+pasto_labels = np.ones(len(pasto_patches))
+cielo_labels = np.ones(len(cielo_patches)) * 2
 
 # Join dataset along with y labels
-x = np.concatenate((vaca_cropped_dataset, pasto_cropped_dataset, cielo_cropped_dataset))
-y = np.concatenate((np.zeros(len(vaca_cropped_dataset)), np.ones(len(pasto_cropped_dataset)), np.ones(len(cielo_cropped_dataset)) * 2))
+train_patches = np.concatenate((vaca_patches, pasto_patches, cielo_patches))
+train_labels = np.concatenate((vaca_labels, pasto_labels, cielo_labels))
 
-X, Y = shuffle(x, y, random_state=42)
-
+# Shuffle dataset
+shuffled_indices = np.random.permutation(len(train_patches))
+patches_set = train_patches[shuffled_indices]
+labels_set = train_labels[shuffled_indices]
 
 # Split dataset into train and test
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+train_quota = 0.8
+train_patches = patches_set[:int(len(patches_set) * train_quota)]
+train_labels = labels_set[:int(len(labels_set) * train_quota)]
+
+test_patches = patches_set[int(len(patches_set) * train_quota):]
+test_labels = labels_set[int(len(labels_set) * train_quota):]
+
+# Train the SVM
+print("Training SVM...")
+clf = svm.SVC(kernel='linear') # linear, poly, rbf, sigmoid
+clf.fit(train_patches, train_labels)
+
+# Store trained model
+model_name = "linear_C_1"
+filename = f"{model_name}_svm_model.pkl"
+joblib.dump(clf, filename)
+
+# Predict on test images and create confusion matrix
+print("Testing SVM...")
+# Make predictions on the test set
+y_pred = clf.predict(test_patches)  # Replace 'clf' with the name of your trained SVM classifier
+
+# Create a confusion matrix
+confusion_mat = confusion_matrix(test_labels, y_pred)
+
+# Print the confusion matrix
+print("Confusion Matrix:")
+print(confusion_mat)
+
+""" # Predict on a new image
+print("Predicting on a new image...")
+
+output_image = classify_image(cow, clf)
+
+# Show the output image
+cv2.imshow('Output', output_image)
+cv2.waitKey(0) """
