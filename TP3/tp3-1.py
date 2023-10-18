ed88dbbef79e67c0e3453ee149c639f3e25df26f
IlonaBrst <92148309+IlonaBrst@users.noreply.github.com>
2023-10-18 13:44:55 -0300

    PART B

--- TP3/tp3-1.py
+++ TP3/tp3-1.py
@@ -1,115 +1,105 @@
 import numpy as np
-
-#Pregunta 1
-n_items = 100
-b0 = 0.5
-b1 = -1
-b2 = 2
-
-# Generate data
-np.random.seed(0)  # Pour la reproductibilité
-data = np.random.rand(n_items, 2) * 5
-
-# Linearly separable labels
-labels = [1 if (b0 + b1 * point[0] + b2 * point[1]) > 0 else -1 for point in data]
+import matplotlib.pyplot as plt
 
 
-# Perceptrón simple
-def perceptron_simple(data, labels, epochs=100, learning_rate=0.01):
+
+def calcular_error(X, y, w):
+    pred = np.sign(np.dot(X, w))
+    error = np.sum(pred != y)
+    return error
+
+def perceptron_simple(X, y, COTA=1000):
+    p, n = X.shape
+    w = np.zeros(n)
+    b = 0  
+    w_min = np.copy(w)
+    b_min = b
+    error_min = float('inf')
+    
+    for i in range(COTA):
+        idx = np.random.randint(0, p)  # random sample
+        xi, yi = X[idx], y[idx]
+
+        h = np.dot(xi, w) + b
+        if np.sign(h) != yi:
+            delta_w = yi * xi
+            w += delta_w
+            b += yi  # update bias
+
+            error = calcular_error(np.hstack((np.ones((p, 1)), X)), y, np.hstack(([b], w)))
+            if error < error_min:
+                error_min = error
+                w_min = np.copy(w)
+                b_min = b  # update minimum biais
+
+    return w_min, b_min  
+
+
+
+def perceptron_optimal(data, labels, epochs=100, learning_rate=0.01):
     w = np.random.rand(2)
     b = 0
     
     for epoch in range(epochs):
+        total_error = 0
         for point, label in zip(data, labels):
-            pred = np.dot(point, w) + b # prediction
-            if np.sign(pred) != label:  # bad prediction?
+            pred = np.dot(point, w) + b
+            if np.sign(pred) != label: # misclassified point
+                total_error += abs(label - pred) # error is the distance from the point to the line
                 w += learning_rate * label * point # update weights
-                b += learning_rate * label # update bias
+                b += learning_rate * label  # update bias
+        if total_error == 0:  # if no error, stop
+            break
     return w, b
 
 
 
 
-#Pregunta 3
-
-# Generate data
-data2 = np.random.rand(n_items, 2) * 5
-
-# Linearly separable labels + noise
-labels2 = []
-for point in data2:
-    # compute distance to line
-    distance_to_line = abs(b0 + b1 * point[0] + b2 * point[1]) / np.sqrt(b1**2 + b2**2)
-    
-    label = 1 if (b0 + b1 * point[0] + b2 * point[1]) > 0 else -1
-
-    # modif some label = noise
-    if 0.5 < distance_to_line < 1.0:
-        label *= -1
-    labels2.append(label)
 
 
 
+def plot_comparison(data, labels, w_normal, b_normal, w_optimized, b_optimized):
+    plt.figure(figsize=(10, 7))
 
-# Training perceptron 
+    # Plotting the data points
+    plt.scatter(data[labels == 1][:, 0], data[labels == 1][:, 1], c='blue', label='Class 1')
+    plt.scatter(data[labels == -1][:, 0], data[labels == -1][:, 1], c='red', label='Class -1')
 
-w, b = perceptron_simple(data, labels)
-w2, b2 = perceptron_simple(data2, labels2)
+    # Perceptron normal line
+    x_values = np.linspace(min(data[:,0]), max(data[:,0]), 100)
+    y_values_normal = (-w_normal[0] * x_values - b_normal) / w_normal[1]
+    plt.plot(x_values, y_values_normal, 'g--', label='Perceptron normal')
 
+    # Perceptron optimized line
+    y_values_optimized = (-w_optimized[0] * x_values - b_optimized) / w_optimized[1]
+    plt.plot(x_values, y_values_optimized, 'c-', label='Perceptron optimized')
 
-
-
-def svm_train(data, labels, learning_rate=0.01, lambda_param=0.01, epochs=1000):
-    w = np.zeros(data.shape[1])
-    for epoch in range(epochs):
-        for i, x in enumerate(data):
-            if (labels[i] * np.dot(data[i], w)) < 1: # misclassified
-                w = w + learning_rate * ( (data[i] * labels[i]) + (-2 * lambda_param * w))
-            else:
-                w = w + learning_rate * (-2 * lambda_param * w) # update weight
-    return w
-
-# Entrainer SVM sur TP3-1
-w_svm1 = svm_train(data, labels)
-
-# Entrainer SVM sur TP3-2
-w_svm2 = svm_train(data2, labels2)
-
-
-# Les SVM fourniront généralement une précision meilleure ou égale par rapport au perceptron simple, surtout quand il y a des points mal classifiés ou que l'ensemble de données n'est pas complètement linéairement séparable.
-
-
-import matplotlib.pyplot as plt
-
-def plot_hyperplane(data, labels, w, b, title=""):
-    plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='jet')
-    ax = plt.gca()
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
-
-    xx = np.linspace(xlim[0], xlim[1])
-    yy = (-w[0] * xx - b) / w[1]
-    
-    plt.plot(xx, yy, 'k-')
-    
-    plt.title(title)
-    plt.xlabel('X1')
-    plt.ylabel('X2')
+    plt.title('Comparison between Normal and Optimized Perceptron')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.legend()
     plt.grid(True)
     plt.show()
 
 
+n_items = 500
+b0 = 0.5
+b1 = -1
+b2 = 2
 
-plot_hyperplane(data, labels, w, b, "Hyperplan du perceptron  TP3-1")
-plot_hyperplane(data2, labels2, w2, b2, "Hyperplan du perceptron TP3-2")
+# Generate data
+np.random.seed(2300)  
+data = np.random.rand(n_items, 2) * 5
+
+# Linearly separable labels
+labels = np.array([1 if (b0 + b1 * point[0] + b2 * point[1]) > 0 else -1 for point in data])
 
 
-plot_hyperplane(data, labels, w_svm1, 0, "Hyperplan SVM  TP3-1")
+w_1,b_1 = perceptron_simple(data, labels)
+w_optimal, b_optimal = perceptron_optimal(data, labels)
 
-
-plot_hyperplane(data2, labels2, w_svm2, 0, "Hyperplan SVM  TP3-2")
-
-
+#Results
+plot_comparison(data, labels, w_1,b_1, w_optimal, b_optimal)
 
 
 
